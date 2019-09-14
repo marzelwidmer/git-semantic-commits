1e49d987fe7354f98453cd6a927dfedad462001f
Marcel Widmer <marzelwidmer@gmail.com>
2019-09-14 18:13:07 +0200

    feat: semver perf support 

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ if [[ -n $1 ]] && [[ $1 == '--scripts' ]]; then
 else
   echo 'Installing git aliases…'
 
-  semantic_aliases=( 'chore' 'docs' 'feat' 'fix' 'localize' 'chore' 'refactor' 'style' 'test' )
+  semantic_aliases=( 'chore' 'docs' 'feat' 'fix' 'localize' 'chore' 'refactor' 'style' 'test' 'perf' )
 
   for semantic_alias in "${semantic_aliases[@]}"; do
     register_git_alias $semantic_alias
@@ -73,4 +73,4 @@ fi
 
 echo
 echo 'Done! Now you can use semantic commits.'
-echo 'See: https://github.com/fteem/git-semantic-commits for more information.'
+echo 'See: https://github.com/marzelwidmer/git-semantic-commits for more information.'
