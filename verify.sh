ee96f7108a1e5c8fa2ee420842450f6cdd6775f5
Marcel Widmer <marzelwidmer@gmail.com>
2019-09-15 08:23:39 +0200

    fix: semver also working with noice commits

--- verify.sh
+++ verify.sh
@@ -34,20 +34,20 @@ VNUM1=${VERSION_BITS[0]}
 VNUM2=${VERSION_BITS[1]}
 VNUM3=${VERSION_BITS[2]}
 
-MAJOR_COUNT_OF_COMMIT_MSG=`git log -1 --pretty=%B | egrep -c '^(breaking|major|BREAKING CHANGE)'`
-MINOR_COUNT_OF_COMMIT_MSG=`git log -1 --pretty=%B | egrep -c '^(feature|minor|feat)'`
-PATCH_COUNT_OF_COMMIT_MSG=`git log -1 --pretty=%B | egrep -c '^(fix|patch|docs|style|refactor|perf|test|chore)'`
-if [ $MAJOR_COUNT_OF_COMMIT_MSG -gt 0 ]; then
-    VNUM1=$((VNUM1+1))
-    VNUM2=0
-    VNUM3=0
+MAJOR_COUNT_OF_COMMIT_MSG=`git log $GIT_REV_LIST --pretty=%B | egrep -c '^(breaking|major|BREAKING CHANGE)'`
+MINOR_COUNT_OF_COMMIT_MSG=`git log $GIT_REV_LIST --pretty=%B | egrep -c '^(feature|minor|feat)'`
+PATCH_COUNT_OF_COMMIT_MSG=`git log $GIT_REV_LIST --pretty=%B | egrep -c '^(fix|patch|docs|style|refactor|perf|test|chore)'`
+if [ $PATCH_COUNT_OF_COMMIT_MSG -gt 0 ]; then
+    VNUM3=$((VNUM3+1))
 fi
 if [ $MINOR_COUNT_OF_COMMIT_MSG -gt 0 ]; then
     VNUM2=$((VNUM2+1))
     VNUM3=0
 fi
-if [ $PATCH_COUNT_OF_COMMIT_MSG -gt 0 ]; then
-    VNUM3=$((VNUM3+1)) 
+if [ $MAJOR_COUNT_OF_COMMIT_MSG -gt 0 ]; then
+    VNUM1=$((VNUM1+1))
+    VNUM2=0
+    VNUM3=0
 fi
 # count all commits for a branch
 GIT_COMMIT_COUNT=`git rev-list --count HEAD`
